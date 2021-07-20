c813181b43d9980be73323414a626405fb0774fe
Ayesha Fathima <86558577+ayesha-fathima@users.noreply.github.com>
2021-07-21 00:18:35 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {margin:0;}
+.nav{
+    margin-bottom:12px;
+}
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #116466;
+  position: fixed;
+  top: 0;
+  width: 100%;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color:#FFCB9A;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover:not(.title) {
+  background-color: #D1E8E2;
+}
+.title {
+  background-color: #2C3531;
+}
+
+</style>
+</head>
+<body>
+   <nav class="Nav">
+    <ul>
+
+    <li>
+    <a href="index.php"><b>Home</b></a>
+    </li>
+    <li>
+    <a href="aboutus.php"><b>About Us</a>
+    </li>
+    <li>
+    <a href="ViewCustomers.php">Customers Details</a>
+    </li>
+    <li>
+    <a href="Transfer.php">Fund Transfer</a>
+    </li>
+    <li>
+    <a href="RecordsPage.php">Transaction History</a>
+    </li>
+    </ul>
+   </nav>
+<body>
+</html>
